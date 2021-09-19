ab7e8bee4eadbd3f27b23c156c2f7427fc78f435
Lord <lord.nemo@protonmail.com>
2021-09-19 20:12:16 +0300

    small fix to pushkin-capybara

--- cppartgenerator.py
+++ cppartgenerator.py
@@ -2,4 +2,5 @@ inp = open("pushkin-capybara", "r")
 
 for line in inp:
     if line != '\n':
-        print('message("' + line[10:-10] + '")')
+        # print('message("' + line[10:-10] + '")')
+        print(line[10:-10])
